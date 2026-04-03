b675d102ae3aaab1b2f2516c7d8464c8a995fb13
Clepa <93273059+MeisterClepa@users.noreply.github.com>
2026-04-03 14:08:44 +0000

    test

--- functions/SpaceArticles/searchArticles/code.js
+++ functions/SpaceArticles/searchArticles/code.js
@@ -4,7 +4,7 @@ url: "https://api.spaceflightnewsapi.net/v4/articles/",
 params: {
     // Передаем в API-запрос параметр query
     search: query,
-    ordering: "-published_at"
+    ordering: "-published_at233"
 },
 headers: {}
 })
